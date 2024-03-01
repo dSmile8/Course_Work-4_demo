979af5268c7e842dd56abdfbdbc9a6d1607b2253
Сергей Арефьев <Simpson6515@yandex.ru>
2024-03-01 15:37:38 +0500

    final fixes

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-from src.hhru import WorkWithFile, HeadHunterAPI, VacanciesManager
-from src.top import TopN
-
-
-def user_interaction():
-    search_query = input("Введите поисковый запрос: ")
-    top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-    schedule = input("Введите ключевые слова для фильтрации вакансий по графику работы:\n"
-                     "Например: Полный день / Вахта / Удаленная ").lower().split()
-    work_with_file = WorkWithFile()
-    top = TopN(top_n, schedule)
-    vacancies = HeadHunterAPI().get_vacancies(search_query)
-    work_with_file.save_to_json(vacancies)
-    vacancies_manager = VacanciesManager()
-    sorted_list = vacancies_manager.sorted_list()
-    work_with_file.save_to_json(sorted_list)
-    vacancies_manager.data_from_json()
-    top.get_top_n()
-
+from src.user_interaction_func import user_interaction
 
 if __name__ == "__main__":
     user_interaction()
